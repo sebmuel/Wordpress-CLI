23dab80ecac9e935f9c5131fb8352e1e73a5a29d
sebmuel <mueller.sebastian117@protonmail.com>
2022-07-12 17:54:02 +0000

    Start Fresh

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import json
+import typer
+import os
+
 """
 functions to load the config
 """
-def 
+
+
+def load_config():
+    __location__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    with open(os.path.join(__location__, "config.json")) as conf:
+        config = json.load(conf)
+    return config
+
+
+config = load_config()
+print(config)
